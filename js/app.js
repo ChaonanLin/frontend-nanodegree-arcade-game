6d6e93c030350b4b4bd3df7ddf359272c504d98b
Savoir <savoir.cn@gmail.com>
2018-09-26 22:38:26 -0700

    set enemy position and speed

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    //Setting the position
+    this.x = x;
+    this.y = y;
+    //Setting the speed
+    this.speed = speed;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = this.x + dt*this.speed;
 };
 
 // Draw the enemy on the screen, required method for game
